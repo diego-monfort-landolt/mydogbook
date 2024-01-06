309746ae42daa95cdc100d868e190978f864dd20
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-01-06 16:35:44 +0100

    change routes

--- my-dogapp/src/app/app-routing.module.ts
+++ my-dogapp/src/app/app-routing.module.ts
@@ -4,7 +4,14 @@ import { AboutComponent } from "./about/about.component";
 import { AppComponent } from "./app.component";
 
 const routes: Routes = [
- 
+  {
+    path: '',
+    component: AppComponent
+  },
+  {
+    path: 'about/:variable',
+    component: AboutComponent
+  }
 ];
 
 @NgModule({
