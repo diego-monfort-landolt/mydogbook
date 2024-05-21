478856908e7d790ff01e18e2aab8523a3cbbd6f9
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-05-21 20:44:04 +0200

    removed spaces and read code

--- my-dogapp/src/app/home/home.component.spec.ts
+++ my-dogapp/src/app/home/home.component.spec.ts
@@ -5,7 +5,6 @@ import { HomeComponent } from './home.component';
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HomeComponent]
